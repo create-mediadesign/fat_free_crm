fe9af92e422006eb40200e969692f4dea51a2d6c
Philipp Ullmann <philipp.ullmann@create.at>
2013-02-08 13:20:39 +0100

    Generate secret token

--- config/recipes/ffc.rb
+++ config/recipes/ffc.rb
@@ -1,7 +1,8 @@
 namespace :ffc do
   desc 'Generate fat free crm configuration app files.'
   task :setup, :roles => :app do
-    config 'settings.default.yml', "#{shared_path}/config/settings.yml"
+    config   'settings.default.yml', "#{shared_path}/config/settings.yml"
+    template 'secret_token.rb.erb',  "#{shared_path}/config/initializers/secret_token.rb"
   end
   after 'deploy:setup', 'ffc:setup'
   
